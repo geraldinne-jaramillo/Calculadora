bec281ee1cfdefd5f65aee2ee5444585caab9405
Geraldinne Paola Jaramillo Avendano <geraldinne.jaramillo@globant.com>
2025-08-12 11:27:29 -0500

    Feat, se crea el proceso de divivir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,40 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("➖Dividamos 2 números➖");
+
+        try {
+            System.out.print("\uD83E\uDD16 Digita el primer número entero ");
+            Integer num1 = scanner.nextInt();
+
+            System.out.print("\uD83E\uDD16 Digita el segundo número entero ");
+            Integer num2 = scanner.nextInt();
+
+            Integer resultado = dividirDosNumeros(num1,num2);
+            System.out.println("El resultado de la división es: " +resultado);
+
+    }catch (InputMismatchException error) {
+        System.out.println("❌ Error: Utilizaste una entrada prohibida ❌");
+    } catch (Exception error) {
+        System.out.println("upss, " +error.getMessage());
+    }finally {
+        System.out.println("Gracias, vuelva pronto.");
+        scanner.close();
     }
+}
+
+public static Integer dividirDosNumeros (Integer nunm1, Integer num2) {
+    if (num2 <= 0) {
+        throw new ArithmeticException("❌No puedes dividir por cero.");
+    }
+    return num2;
+}
+
 }
